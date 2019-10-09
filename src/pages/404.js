388d2bb23f9d8f1c80e38dc3cd4736536e74cc50
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-10-08 17:40:04 -0700

    structured logs canary

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,9 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const foo = 'bar'
 
 const NotFoundPage = () => (
   <Layout>
@@ -9,6 +11,7 @@ const NotFoundPage = () => (
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
+  
 )
 
 export default NotFoundPage

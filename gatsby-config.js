baee9b5741faa00d00749ae5c9116745a295b0ca
Dustin Schau <dustinschau@gmail.com>
2019-10-21 13:03:08 -0700

    chore: show warning

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,12 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
